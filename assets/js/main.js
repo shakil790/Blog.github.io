cbfcba29726887f28264ce23b4303b386424c10e
shakil790 <63404036+shakil790@users.noreply.github.com>
2020-04-10 23:30:07 +0600

    my project blog publicing
    that's very nice

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	// responsive-menu
+
+    jQuery('#mean-menu').meanmenu({
+        meanMenuContainer: '.responsive-menu',
+        meanScreenWidth: "992"
+    });
+})
